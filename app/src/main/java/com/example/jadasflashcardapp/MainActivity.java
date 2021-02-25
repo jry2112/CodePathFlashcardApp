b3c31de8af5657bb01a22852ccbf776ab0c2a917
younjada <jry2112@columbia.edu>
2021-02-24 20:18:25 -0500

    added question, answer, click to view answer

--- app/src/main/java/com/example/jadasflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/jadasflashcardapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.jadasflashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        // User can tap the text to hide question and show answer
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hide question
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                // Show answer
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
