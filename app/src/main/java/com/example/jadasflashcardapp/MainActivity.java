647b3e5cdc63e5710df2cec53eb3f03ca19f3043
younjada <jry2112@columbia.edu>
2021-02-26 01:58:42 -0500

    used boolean to toggle answers

--- app/src/main/java/com/example/jadasflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/jadasflashcardapp/MainActivity.java
@@ -12,6 +12,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isAnswerShowing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
         TextView correctAnswer = findViewById(R.id.correct_answer);
         TextView wrongAnswer1 = findViewById(R.id.wrong_answer_1);
         TextView wrongAnswer2 = findViewById(R.id.wrong_answer_2);
-        ImageView showAnswers = findViewById(R.id.toggle_choices_visibility_down);
-        ImageView hideAnswers = findViewById(R.id.toggle_choices_visibility_up);
+        ImageView showAnswersIcon = findViewById(R.id.toggle_choices_visibility);
+
+
+
+
+
 
         // User can tap the question text to hide question and show answer
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
@@ -85,37 +90,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-        // User can tap the up triangle to hide answers
-        hideAnswers.setOnClickListener(new View.OnClickListener() {
+        // Toggle show answer icon based on click
+        showAnswersIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Hide answers
-                correctAnswer.setVisibility(View.INVISIBLE);
-                wrongAnswer1.setVisibility(View.INVISIBLE);
-                wrongAnswer2.setVisibility(View.INVISIBLE);
-                // Show down triangle
-                showAnswers.setVisibility(View.VISIBLE);
-                // Hide up triangle
-                hideAnswers.setVisibility(View.INVISIBLE);
-
-            }
-
-        });
-
-        // User can tap the down triangle to show answers
-        showAnswers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Hide answers
-                correctAnswer.setVisibility(View.VISIBLE);
-                wrongAnswer1.setVisibility(View.VISIBLE);
-                wrongAnswer2.setVisibility(View.VISIBLE);
-                // Show up triangle
-                hideAnswers.setVisibility(View.VISIBLE);
-                // Hide down triangle
-                showAnswers.setVisibility(View.INVISIBLE);
-
+                if (isAnswerShowing){
+                    showAnswersIcon.setImageResource(R.drawable.care_up_thin);
+                    correctAnswer.setVisibility(View.VISIBLE);
+                    wrongAnswer1.setVisibility(View.VISIBLE);
+                    wrongAnswer2.setVisibility(View.VISIBLE);
+                }
+                else{
+                    showAnswersIcon.setImageResource(R.drawable.care_down_thin);
+                    correctAnswer.setVisibility(View.INVISIBLE);
+                    wrongAnswer1.setVisibility(View.INVISIBLE);
+                    wrongAnswer2.setVisibility(View.INVISIBLE);
+                }
+                isAnswerShowing = !isAnswerShowing;
             }
 
         });
